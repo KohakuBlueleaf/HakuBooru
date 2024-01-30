3a1eb87c50538f900e40e4dce99662064d720279
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2024-01-30 23:08:08 +0800

    Improve query post from tags

--- hakubooru/dataset/utils.py
+++ hakubooru/dataset/utils.py
@@ -1,14 +1,24 @@
 from numpy import base_repr
+from peewee import ModelSelect
+
 from .db import *
 
 
-def get_post_by_tag(tag: Tag):
-    id_str = f"${base_repr(tag.id, 36)}#"
-    return (
-        Post.select()
-        .join(PostFTS, on=(Post.id == PostFTS.rowid))
-        .where(PostFTS.tag_list.contains(id_str))
-    )
+def get_post_by_tags(tag: Tag | list[Tag]) -> ModelSelect:
+    if isinstance(tag, Tag):
+        id_str = f"${base_repr(tag.id, 36)}#"
+        return (
+            Post.select()
+            .join(PostFTS, on=(Post.id == PostFTS.rowid))
+            .where(PostFTS.tag_list.contains(id_str))
+        )
+    else:
+        id_str = " OR ".join(f'"${base_repr(t.id, 36)}#"' for t in tag)
+        return (
+            Post.select()
+            .join(PostFTS, on=(Post.id == PostFTS.rowid))
+            .where(PostFTS.tag_list.match(id_str))
+        )
 
 
 def get_tag_by_name(name: str):
