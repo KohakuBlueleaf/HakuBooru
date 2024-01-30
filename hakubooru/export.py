eef8bcac7a1d35e81253a4c693d6e80e4bb02e92
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2024-01-30 22:39:57 +0800

    Support 3-stage updating system.

--- hakubooru/export.py
+++ hakubooru/export.py
@@ -74,13 +74,38 @@ class Exporter:
 
         self.do_caption = captioner is not None
 
+    def process_data(self, data_id, data, post_dict):
+        fail = success = 0
+        try:
+            ext, content = list(data.items())[-1]
+            if self.do_caption:
+                caption = self.captioner.caption(post_dict[data_id], content)
+            else:
+                caption = None
+
+            self.saver(
+                data_id,
+                content,
+                ext,
+                caption,
+            )
+        except Exception as e:
+            logger.warning(
+                f"Error occured when doing captioning and saving {data_id}: {e}"
+            )
+            fail = 1
+        else:
+            del post_dict[data_id]
+            success = 1
+        return data, success, fail
+
     def export_posts(self, choosed_posts: list[Post]):
         dataset_dir = self.dataset_dir
 
         existed_tar = {
-            int(file_id_regex.match(f).group(1)): os.path.join(dataset_dir, f).replace(
-                "\\", "/"
-            )
+            int(file_id_regex.match(f).group(1)): [
+                os.path.join(dataset_dir, f).replace("\\", "/")
+            ]
             for f in os.listdir(dataset_dir)
             if f.endswith(".tar")
         }
@@ -94,33 +119,64 @@ class Exporter:
             id_map[bucket_id][post.id] = post
         id_map = {k: id_map[k] for k in sorted(id_map)}
 
-        not_found = set()
+        bucket_not_found = set()
         success = 0
+        fail = 0
         for bucket_id, post_dict in tqdm(id_map.items()):
-            if bucket_id not in existed_tar:
-                not_found.add(bucket_id)
+            if bucket_id not in existed_tar and bucket_id + 1000 not in existed_tar:
+                bucket_not_found.add(bucket_id)
                 continue
 
-            dataset = wds.WebDataset(existed_tar[bucket_id])
+            # Concat main and sub dataset together
+            dataset = wds.WebDataset(
+                existed_tar.get(bucket_id, []) + existed_tar.get(bucket_id + 1000, []),
+            )
 
+            data_cache = {}
             for data in iter(dataset):
                 data_id = int(data["__key__"])
                 if data_id in post_dict:
-                    ext, content = list(data.items())[-1]
-                    if self.do_caption:
-                        caption = self.captioner.caption(post_dict[data_id], content)
-                    else:
-                        caption = None
+                    data_cache[data_id] = data
 
-                    self.saver(
-                        data_id,
-                        content,
-                        ext,
-                        caption,
-                    )
-                    success += 1
+            for data_id, data in data_cache.items():
+                data, s, f = self.process_data(data_id, data, post_dict)
+                success += s
+                fail += f
 
-        logger.warning(
-            f"Some shards of the dataset are not found: {sorted(not_found)}, The are all ignored"
-        )
-        logger.info(f"Exported {success} images")
+        remains = {}
+        for _, post_dict in id_map.items():
+            remains.update(post_dict)
+
+        # Check addon dataset if needed
+        if remains and 2000 in existed_tar:
+            addon_dataset = wds.WebDataset(existed_tar[2000])
+            data_cache = {}
+            for data in iter(addon_dataset):
+                data_id = int(data["__key__"])
+                if data_id in remains:
+                    data_cache[data_id] = data
+            for data_id, data in data_cache.items():
+                data, s, f = self.process_data(data_id, data, post_dict)
+                success += s
+                fail += f
+        post_not_found = len(remains)
+
+        if bucket_not_found:
+            logger.warning(
+                f"{len(bucket_not_found)} posts are not exported"
+                "because the bucket doesn't exist"
+            )
+
+        if post_not_found:
+            logger.warning(
+                f"{post_not_found} posts are not found in the dataset\n"
+                "This can be caused by missing/outdating dataset files or corrupted dataset files"
+            )
+
+        if fail:
+            logger.warning(
+                f"{fail} images failed to captioning and save\n"
+                "please check your settings on captioner and saver."
+            )
+
+        logger.info(f"Successfully exported {success} images")
