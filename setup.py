e7845719592ca8f3d7e033790dbdf06026b2c4b5
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2024-02-03 17:12:57 +0800

    dev3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="hakubooru",
     packages=find_packages(),
-    version="0.0.1.dev1",
+    version="0.0.1.dev3",
     license="Apache 2.0",
     url="https://github.com/KohakuBlueleaf/HakuBooru",
     description="Text-image dataset maker for anime-style images",
