2709f780b007d0a5f431abbbe21d4416cc7d2d0a
Makki Shizu <142442886+MakkiShizu@users.noreply.github.com>
2024-07-12 00:43:15 +0800

    A simple GUI (#1)
    * updated gui
    
    * updated demo image
    
    * Make Gradio an optional dependency and format code with Black

---
+++ gui/gui.py
@@ -0,0 +1,67 @@
+import gradio as gr
+from haku_character import haku_character
+
+
+def gradio_interface(
+    names,
+    required,
+    max,
+    ratings,
+    db_path,
+    image_path,
+    output_path,
+    id_range_min,
+    id_range_max,
+):
+    names_list = [name.strip() for name in names.split(",")]
+    required_list = [req.strip() for req in required.split(",")]
+    max_image = int(max)
+    ratings_list = [int(r.strip()) for r in ratings.split(",")]
+    db_path = "./data/danbooru2023.db"
+    image_path = "./images"
+    output_path = "./train_data"
+    id_min = int(id_range_min)
+    id_max = int(id_range_max)
+    log_contents = haku_character(
+        names_list,
+        required_list,
+        max_image,
+        ratings_list,
+        db_path,
+        image_path,
+        output_path,
+        id_min,
+        id_max,
+    )
+    return log_contents
+
+
+iface = gr.Interface(
+    fn=gradio_interface,
+    inputs=[
+        gr.Textbox(
+            lines=5,
+            placeholder="Enter names separated by commas,such as: kamisato_ayaka,seele_vollerei",
+        ),  # 名字列表
+        gr.Textbox(
+            lines=1,
+            placeholder="Enter required tags separated by commas,such as: solo,highres",
+        ),  # 必要的标签
+        gr.Number(value=200, label="Enter max number"),  # 最大数量
+        gr.Textbox(value="0,1,2", label="Enter rating number(0~3)"),  # 评分，越小越安全
+        gr.Textbox(
+            lines=1, value="./data/danbooru2023.db", placeholder="Enter database path"
+        ),  # 数据库路径
+        gr.Textbox(
+            lines=1, value="./images", placeholder="Enter image path"
+        ),  # 图片路径
+        gr.Textbox(
+            lines=1, value="./train_data", placeholder="Enter output path"
+        ),  # 输出路径
+        gr.Number(value="0", label="Enter id min range"),  # id范围
+        gr.Number(value="8000000", label="Enter id max range"),  # id范围
+    ],
+    outputs="text",  # 输出为文本，显示结果
+)
+
+iface.launch(share=True, server_port=2104)
---
+++ gui/haku_character.py
@@ -0,0 +1,154 @@
+import logging
+import random
+import re
+import operator
+
+from io import StringIO
+from functools import reduce
+from hakubooru.caption import KohakuCaptioner
+from hakubooru.dataset import load_db, Post
+from hakubooru.dataset.utils import (
+    select_post_by_tags,
+    select_post_by_required_tags,
+    get_tag_by_name,
+)
+from hakubooru.export import Exporter, FileSaver
+from hakubooru.logging import logger
+from hakubooru.source import TarSource
+
+
+def haku_character(
+    names,
+    required,
+    max,
+    ratings,
+    db_path,
+    image_path,
+    output_path,
+    id_range_min,
+    id_range_max,
+):
+    log_stream = StringIO()
+    stream_handler = logging.StreamHandler(log_stream)
+    logger.addHandler(stream_handler)
+
+    logger.setLevel(logging.INFO)
+    logger.info("Loading danbooru2023.db")
+    load_db(db_path)
+
+    logger.info("Querying posts")
+
+    required_tags = [get_tag_by_name(tag) for tag in required]
+
+    for name in names:
+        target_characters = [
+            get_tag_by_name(tag)
+            for tag in [
+                "{name}".format(name=name),
+            ]
+        ]
+
+        choosed_post_characters = select_post_by_tags(target_characters)
+        choosed_post_required = select_post_by_required_tags(required_tags)
+        choosed_post = choosed_post_required.where(
+            Post.id << choosed_post_characters
+        ).where(Post.id > id_range_min, Post.id < id_range_max)
+        choosed_post = list(choosed_post)
+
+        if len(choosed_post) < max:
+            rp = max / len(choosed_post)
+            rp = round(rp)
+
+            logger.info(f"Found {len(choosed_post_characters)} posts for characters")
+            logger.info(f"Found {len(choosed_post_required)} posts for required tags")
+            logger.info(f"Found {len(choosed_post)} posts")
+
+            logger.info("Build exporter")
+            clean_name = re.sub(r"[^\w\s]", "", name)
+            exporter = Exporter(
+                source=TarSource(image_path),
+                saver=FileSaver(
+                    "{output_path}/{clean_name}/{rp}_data".format(
+                        output_path=output_path, clean_name=clean_name, rp=rp
+                    )
+                ),
+                captioner=KohakuCaptioner(),
+                process_batch_size=250,
+                process_threads=4,
+            )
+
+            exporter.export_posts(choosed_post)
+            continue
+
+        x = 100
+
+        while True:
+            choosed_post = (
+                choosed_post_required.where(Post.id << choosed_post_characters)
+                .where(Post.id > id_range_min, Post.id < id_range_max)
+                .where(Post.score >= x)
+                .where(reduce(operator.or_, (Post.rating == r for r in ratings)))
+            )
+            choosed_post = list(choosed_post)
+
+            if len(choosed_post) >= max:
+                break
+
+            if x < 0:
+                break
+
+            x -= 10
+
+        if len(choosed_post) >= max:
+            choosed_post = random.sample(choosed_post, max)
+
+        logger.info(f"Found {len(choosed_post_characters)} posts for characters")
+        logger.info(f"Found {len(choosed_post_required)} posts for required tags")
+        logger.info(f"Found {len(choosed_post)} posts")
+
+        logger.info("Build exporter")
+        clean_name = re.sub(r"[^\w\s]", "", name)
+        exporter = Exporter(
+            source=TarSource(image_path),
+            saver=FileSaver(
+                "{output_path}/{clean_name}/1_data".format(
+                    output_path=output_path, clean_name=clean_name
+                )
+            ),
+            captioner=KohakuCaptioner(),
+            process_batch_size=250,
+            process_threads=4,
+        )
+
+        exporter.export_posts(choosed_post)
+
+    logger.removeHandler(stream_handler)
+    log_contents = log_stream.getvalue()
+    log_stream.close()
+
+    return log_contents
+
+
+if __name__ == "__main__":
+    names = ["kamisato_ayaka"]
+    required = ["solo", "highres", "genshin_impact"]
+    max = 200
+    ratings = [0, 1, 2]
+    db_path = "./data/danbooru2023.db"
+    image_path = "./images"
+    output_path = "./train_data"
+    id_range_min = 5000000
+    id_range_max = 8000000
+    print(
+        haku_character(
+            names,
+            required,
+            max,
+            ratings,
+            db_path,
+            image_path,
+            output_path,
+            id_range_min,
+            id_range_max,
+        )
+    )
