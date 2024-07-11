b9c74f825fb32b0fd161277660860f47fe6cc9ca
MakkiShizu <2104407774@qq.com>
2024-07-12 00:08:23 +0800

    Make Gradio an optional dependency and format code with Black

--- gui/haku_character.py
+++ gui/haku_character.py
@@ -16,7 +16,18 @@ from hakubooru.export import Exporter, FileSaver
 from hakubooru.logging import logger
 from hakubooru.source import TarSource
 
-def haku_character(names,required,max,ratings,db_path,image_path,output_path,id_range_min,id_range_max):
+
+def haku_character(
+    names,
+    required,
+    max,
+    ratings,
+    db_path,
+    image_path,
+    output_path,
+    id_range_min,
+    id_range_max,
+):
     log_stream = StringIO()
     stream_handler = logging.StreamHandler(log_stream)
     logger.addHandler(stream_handler)
@@ -27,9 +38,7 @@ def haku_character(names,required,max,ratings,db_path,image_path,output_path,id_
 
     logger.info("Querying posts")
 
-    required_tags = [
-        get_tag_by_name(tag) for tag in required
-    ]
+    required_tags = [get_tag_by_name(tag) for tag in required]
 
     for name in names:
         target_characters = [
@@ -41,11 +50,13 @@ def haku_character(names,required,max,ratings,db_path,image_path,output_path,id_
 
         choosed_post_characters = select_post_by_tags(target_characters)
         choosed_post_required = select_post_by_required_tags(required_tags)
-        choosed_post = choosed_post_required.where(Post.id << choosed_post_characters).where(Post.id > id_range_min, Post.id < id_range_max)
+        choosed_post = choosed_post_required.where(
+            Post.id << choosed_post_characters
+        ).where(Post.id > id_range_min, Post.id < id_range_max)
         choosed_post = list(choosed_post)
 
         if len(choosed_post) < max:
-            rp = max/len(choosed_post)
+            rp = max / len(choosed_post)
             rp = round(rp)
 
             logger.info(f"Found {len(choosed_post_characters)} posts for characters")
@@ -53,10 +64,14 @@ def haku_character(names,required,max,ratings,db_path,image_path,output_path,id_
             logger.info(f"Found {len(choosed_post)} posts")
 
             logger.info("Build exporter")
-            clean_name = re.sub(r'[^\w\s]', '', name)
+            clean_name = re.sub(r"[^\w\s]", "", name)
             exporter = Exporter(
                 source=TarSource(image_path),
-                saver=FileSaver("{output_path}/{clean_name}/{rp}_data".format(output_path=output_path,clean_name=clean_name,rp=rp)),
+                saver=FileSaver(
+                    "{output_path}/{clean_name}/{rp}_data".format(
+                        output_path=output_path, clean_name=clean_name, rp=rp
+                    )
+                ),
                 captioner=KohakuCaptioner(),
                 process_batch_size=250,
                 process_threads=4,
@@ -68,7 +83,12 @@ def haku_character(names,required,max,ratings,db_path,image_path,output_path,id_
         x = 100
 
         while True:
-            choosed_post = choosed_post_required.where(Post.id << choosed_post_characters).where(Post.id > id_range_min, Post.id < id_range_max).where(Post.score >= x).where(reduce(operator.or_, (Post.rating == r for r in ratings)))
+            choosed_post = (
+                choosed_post_required.where(Post.id << choosed_post_characters)
+                .where(Post.id > id_range_min, Post.id < id_range_max)
+                .where(Post.score >= x)
+                .where(reduce(operator.or_, (Post.rating == r for r in ratings)))
+            )
             choosed_post = list(choosed_post)
 
             if len(choosed_post) >= max:
@@ -87,10 +107,14 @@ def haku_character(names,required,max,ratings,db_path,image_path,output_path,id_
         logger.info(f"Found {len(choosed_post)} posts")
 
         logger.info("Build exporter")
-        clean_name = re.sub(r'[^\w\s]', '', name)
+        clean_name = re.sub(r"[^\w\s]", "", name)
         exporter = Exporter(
             source=TarSource(image_path),
-            saver=FileSaver("{output_path}/{clean_name}/1_data".format(output_path=output_path,clean_name=clean_name)),
+            saver=FileSaver(
+                "{output_path}/{clean_name}/1_data".format(
+                    output_path=output_path, clean_name=clean_name
+                )
+            ),
             captioner=KohakuCaptioner(),
             process_batch_size=250,
             process_threads=4,
@@ -101,17 +125,30 @@ def haku_character(names,required,max,ratings,db_path,image_path,output_path,id_
     logger.removeHandler(stream_handler)
     log_contents = log_stream.getvalue()
     log_stream.close()
-    
+
     return log_contents
+
 
 if __name__ == "__main__":
     names = ["kamisato_ayaka"]
-    required = ["solo","highres","genshin_impact"]
+    required = ["solo", "highres", "genshin_impact"]
     max = 200
-    ratings = [0,1,2]
-    db_path = "./HakuBooru/data/danbooru2023.db"
-    image_path = "./HakuBooru/images"
+    ratings = [0, 1, 2]
+    db_path = "./data/danbooru2023.db"
+    image_path = "./images"
     output_path = "./train_data"
     id_range_min = 5000000
     id_range_max = 8000000
-    print(haku_character(names, required, max, ratings, db_path, image_path, output_path, id_range_min, id_range_max))
+    print(
+        haku_character(
+            names,
+            required,
+            max,
+            ratings,
+            db_path,
+            image_path,
+            output_path,
+            id_range_min,
+            id_range_max,
+        )
+    )
--- setup.py
+++ setup.py
@@ -16,4 +16,7 @@ setup(
         "python-dateutil",
         "webdataset",
     ],
+    extra_require={
+        "gui": ["gradio"],
+    },
 )
