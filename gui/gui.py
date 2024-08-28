c09245cccfa3851a92965185e60e3530e41ab0ca
MakkiShizu <2104407774@qq.com>
2024-08-28 20:40:56 +0800

    Changed the page from Interface to Blocks.

--- gui/gui.py
+++ gui/gui.py
@@ -2,7 +2,7 @@ import gradio as gr
 from haku_character import haku_character
 
 
-def gradio_interface(
+def gradio_haku(
     names,
     required,
     max,
@@ -38,48 +38,90 @@ def gradio_interface(
     return log_contents
 
 
-iface = gr.Interface(
-    fn=gradio_interface,
-    inputs=[
-        gr.Textbox(
-            lines=5,
-            value="kamisato_ayaka,seele_vollerei",
-            info="Enter names separated by commas,such as: kamisato_ayaka,seele_vollerei",
-            label="names",
-        ),  # 名字列表
-        gr.Textbox(
-            lines=1,
-            value="solo,highres",
-            info="Enter required tags separated by commas,such as: solo,highres",
-            label="else required tags",
-        ),  # 必要的标签
-        gr.Number(
-            value="-1", label="Enter images max number,-1 means no limit"
-        ),  # 最大数量
-        gr.Textbox(
-            value="0,1,2,3",
-            label="Enter rating number(s) separated by commas,such as: 0,1,2,3, 0 means safe, 3 means nsfw",
-        ),  # 评级，越小越安全
-        gr.Number(
-            value="-1",
-            label="Enter score threshold, -1 means automatically from top to bottom",
-        ),  # danbooru分数
-        gr.Textbox(
-            lines=1, value="./data/danbooru2023.db", placeholder="Enter database path"
-        ),  # 数据库路径
-        gr.Textbox(
-            lines=1, value="./images", placeholder="Enter image path"
-        ),  # 图片路径
-        gr.Textbox(
-            lines=1, value="./train_data", placeholder="Enter output path"
-        ),  # 输出路径
-        gr.Number(value="0", label="Enter id min range"),  # id范围
-        gr.Number(value="10000000", label="Enter id max range"),  # id范围
-        gr.Checkbox(
-            label="add_character_category_path"
-        ),  # 是否根据names中的名称单独新建文件夹
-    ],
-    outputs="text",  # 输出为文本，显示结果
-)
+with gr.Blocks() as blocks:
+    with gr.Row():
+        with gr.Column(scale=1):
+            names = gr.Textbox(
+                lines=5,
+                value="kamisato_ayaka,seele_vollerei",
+                info="Enter tags separated by commas, such as: kamisato_ayaka,seele_vollerei",
+                label="Tags",
+            )  # 名字列表
+            required = gr.Textbox(
+                lines=1,
+                value="solo,highres",
+                info="Enter required tags separated by commas, such as: solo,highres",
+                label="else required tags",
+            )  # 必要的标签
+            max = gr.Number(
+                value="-1",
+                info="Enter images max number, -1 means no limit",
+                label="Max Images",
+            )  # 最大数量
+            ratings = gr.Textbox(
+                value="0,1,2,3",
+                info="Enter rating number(s) separated by commas, such as: 0,1,2,3. 0 means safe, 3 means nsfw",
+                label="Ratings",
+            )  # 评级，越小越安全
+            score = gr.Number(
+                value="-1",
+                info="Enter score threshold, -1 means automatically from top to bottom",
+                label="Score",
+            )  # danbooru分数
+        with gr.Column(scale=2):
+            db_path = gr.Textbox(
+                lines=1,
+                value="./data/danbooru2023.db",
+                info="Enter database path",
+                label="DataBase path",
+            )  # 数据库路径
+            image_path = gr.Textbox(
+                lines=1,
+                value="./images",
+                info="Enter images path",
+                label="Images path",
+            )  # 图片路径
+            output_path = gr.Textbox(
+                lines=1,
+                value="./train_data",
+                info="Enter output path",
+                label="Output path",
+            )  # 输出路径
+            id_range_min = gr.Number(
+                value="0",
+                info="Enter id min range",
+                label="ID Min",
+            )  # id范围
+            id_range_max = gr.Number(
+                value="10000000",
+                info="Enter id max range",
+                label="ID Max",
+            )  # id范围
+            add_character_category_path = gr.Checkbox(
+                info="When enabled, folders will be automatically categorized based on the tags.",
+                label="Add tag category path",
+            )  # 是否根据tags中的名称单独新建文件夹
+    with gr.Row():
+        run_button = gr.Button("Start")
+    with gr.Row():
+        output_log = gr.Textbox(label="Log contents")
 
-iface.launch(share=True, server_port=2104)
+    run_button.click(
+        gradio_haku,
+        inputs=[
+            names,
+            required,
+            max,
+            ratings,
+            score,
+            db_path,
+            image_path,
+            output_path,
+            id_range_min,
+            id_range_max,
+            add_character_category_path,
+        ],
+        outputs=output_log,
+    )
+
+blocks.launch(share=True, server_port=2104)
